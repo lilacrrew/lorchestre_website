e5efb08dc8a6d34a6cd4fff8e0f68811acb4d944
lilacrrew <149533409+lilacrrew@users.noreply.github.com>
2023-12-20 21:23:02 +0600

    Add files via upload

---
+++ src/components/home/team/Team.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import Heading from "../../common/Heading"
+import { team } from "../../data/Data"
+import "./team.css"
+
+const Team = () => {
+  return (
+    <>
+      <section className='team background'>
+        <div className='container'>
+          <Heading title='Our Musicians' />
+
+          <div className='content mtop grid3'>
+            {team.map((val, index) => (
+              <div className='box' key={index}>
+                <div className='details'>
+                  <div className='img'>
+                    <img src={val.cover} alt='' />
+                    <i className='fa-solid fa-circle-check'></i>
+                  </div>
+                  <h4>{val.name}</h4>
+
+                  <ul>
+                    {val.icon.map((icon, index) => (
+                      <li key={index}>{icon}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Team
