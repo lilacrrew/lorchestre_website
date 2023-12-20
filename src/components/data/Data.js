3aa974fea6324a46416c7d8ef8fea182d6b96c61
lilacrrew <149533409+lilacrrew@users.noreply.github.com>
2023-12-20 21:16:05 +0600

    Add files via upload

---
+++ src/components/data/Data.js
@@ -0,0 +1,253 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+/*
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "Concert programmes",
+    total: "122 Property",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "Gallery",
+    total: "155 Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Musicians",
+    total: "300 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Tickets",
+    total: "80 Property",
+  },
+
+]
+*/
+
+const features = [
+  {
+      id: 10,
+      title: "Digital Marketing",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et elit vitae lectus convallis scelerisque. Cras vestibulum blandit lorem, at fringilla nisl sollicitudin ac. Nunc venenatis nec neque sed semper. Mauris viverra, sapien sed fringilla egestas, sem felis condimentum augue, vitae sodales sem metus in ex. Aenean massa velit, sollicitudin quis elementum sit amet, vehicula sed nunc."
+  },
+  {
+      id: 11,
+      title: "Trade Shows",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et elit vitae lectus convallis scelerisque. Cras vestibulum blandit lorem, at fringilla nisl sollicitudin ac. Nunc venenatis nec neque sed semper. Mauris viverra, sapien sed fringilla egestas, sem felis condimentum augue, vitae sodales sem metus in ex. Aenean massa velit, sollicitudin quis elementum sit amet, vehicula sed nunc."
+  },
+  {
+      id: 12,
+      title: "Branding",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et elit vitae lectus convallis scelerisque. Cras vestibulum blandit lorem, at fringilla nisl sollicitudin ac. Nunc venenatis nec neque sed semper. Mauris viverra, sapien sed fringilla egestas, sem felis condimentum augue, vitae sodales sem metus in ex. Aenean massa velit, sollicitudin quis elementum sit amet, vehicula sed nunc."
+  },
+  {
+      id: 13,
+      title: "Content Creation",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et elit vitae lectus convallis scelerisque. Cras vestibulum blandit lorem, at fringilla nisl sollicitudin ac. Nunc venenatis nec neque sed semper. Mauris viverra, sapien sed fringilla egestas, sem felis condimentum augue, vitae sodales sem metus in ex. Aenean massa velit, sollicitudin quis elementum sit amet, vehicula sed nunc."
+  }, 
+  {
+      id: 14,
+      title: "Graphic Design",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et elit vitae lectus convallis scelerisque. Cras vestibulum blandit lorem, at fringilla nisl sollicitudin ac. Nunc venenatis nec neque sed semper. Mauris viverra, sapien sed fringilla egestas, sem felis condimentum augue, vitae sodales sem metus in ex. Aenean massa velit, sollicitudin quis elementum sit amet, vehicula sed nunc."
+  },
+  {
+      id: 15,
+      title: "Media Buying",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et elit vitae lectus convallis scelerisque. Cras vestibulum blandit lorem, at fringilla nisl sollicitudin ac. Nunc venenatis nec neque sed semper. Mauris viverra, sapien sed fringilla egestas, sem felis condimentum augue, vitae sodales sem metus in ex. Aenean massa velit, sollicitudin quis elementum sit amet, vehicula sed nunc."
+  }
+];
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Christmas Concert",
+    location: "Dostyk 23, Almaty",
+    price: "23,000",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "Chaikovski Concert",
+    location: "Zhibek Zholy 1, Astana",
+    price: "19,000",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "The best movie soundtracks",
+    location: "Abay 55, Almaty",
+    price: "20,000",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "Ghibli movies Soundtrack",
+    location: "Dostyk 23, Almaty",
+    price: "$7,540",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "Eric Vans",
+    location: "Ospanova 30, Aktobe",
+    price: "15,000",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "Own Concert for 20years",
+    location: "Zhibek Zholy 12, Astana",
+    price: "25,000",
+  },
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "15 Awards	",
+    name: "All Awards",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "240 musicians",
+    name: "210 musicians and 30 staff",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "200 concerts",
+    name: "For all the time we have already held such concerts",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "500K followers",
+    name: "In socila media",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "New Orleans, Louisiana",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    name: "Jerrsy, United State",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3,
+    name: "Liverpool, London",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-3.png",
+  },
+  {
+    id: 4,
+    name: "NewYork, United States",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-4.png",
+  },
+  {
+    id: 5,
+    name: "Montreal, Canada",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-5.png",
+  },
+  {
+    id: 6,
+    name: "California, USA",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-6.png",
+  },
+]
+export const team = [
+  {
+    list: "50",
+    cover: "../images/customer/team-1.jpg",
+    name: "Sargam S. Singh",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/team-2.jpg",
+    name: "Harijeet M. Siller",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    name: "Anna K. Young",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "51",
+    cover: "../images/customer/team-4.jpg",
+    name: "Michael P. Grimaldo",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/team-5.jpg",
+    name: "Michael P. Grimaldo",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/team-5.jpg",
+    name: "Adam K. Jollio",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+]
+
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+  },
+]
+
+const sections = {nav, features, list, awards, location, team, footer};
+
+export default sections;
