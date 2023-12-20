c8610d6c9d7c5c936c8cae936198de97fd735f3b
lilacrrew <149533409+lilacrrew@users.noreply.github.com>
2023-12-20 21:22:15 +0600

    Add files via upload

---
+++ src/components/home/recent/RecentCard.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { list } from "../../data/Data"
+
+const RecentCard = () => {
+  return (
+    <>
+      <div className='content grid3 mtop'>
+        {list.map((val, index) => {
+          const { cover, location, name, price } = val
+          return (
+            <div className='box shadow' key={index}>
+              <div className='img'>
+                <img src={cover} alt='' />
+              </div>
+              <div className='text'>
+                <div className='category flex'>
+                  <i className='fa fa-heart'></i>
+                </div>
+                <h4>{name}</h4>
+                <p>
+                  <i className='fa fa-location-dot'></i> {location}
+                </p>
+              </div>
+              <div className='button flex'>
+                <div>
+                  <button className='btn2'>{price}</button> <label htmlFor=''></label>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default RecentCard
